f72f563fd58c0190480b3545e1e82ccd21866346
anselmfoester <28807047-anselmfoester@users.noreply.replit.com>
2023-11-10 16:03:56 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Jambit x Hackerschool
+
+This template should help get you started developing with Svelte in Vite.
+
+The entry point for the app can be found in /src/App.svelte.
+
+Press the "Run" to start the App. You can also open the webpage in a new Tab.
---
+++ src/App.svelte
@@ -0,0 +1,66 @@
+<script>
+</script>
+
+<h1>Christoph</h1>
+<br />
+<strong>Alter 15</strong>
+<em>test</em>
+<div>wow</div>
+<a href="google.com">Link</a>
+<br />
+<q>Morgen</q>
+<br />
+<hr />
+<img
+  src="https://www.americanexpress.com/de-de/amexcited/media/cache/default/cms/2022/07/landschaft-slowenien-nationalpark.jpg"
+/>
+<button>Klick hier</button>
+<br />
+<del>Gelöscht halt</del>
+<ins>Gehört auch dazu</ins>
+<br />
+<mark>Hier muss ein Satz rein</mark>
+<input type="text" />
+<br />
+<p id="test">Test</p>
+<a href="amazon.com">
+  <button>Amazon</button>
+</a>
+<br />
+<input type="radio" id="radioid" value="radiowert" name="auswahl" />
+<div>Checkbox</div>
+<input type="checkbox" id="checkboxid" name="checkbox" />
+<br />
+
+<select id="selectid" name="dropdown">
+  <option value="1">True</option>
+  <option value="2">False</option>
+</select>
+
+<iframe
+  src="https://scratch.mit.edu/projects/244698177/embed"
+  allowtransparency="true"
+  width="485"
+  height="402"
+  frameborder="0"
+  scrolling="no"
+  allowfullscreen
+/>
+
+<style>
+  h1 {
+    color: "red";
+  }
+
+  del {
+    background-color: green;
+  }
+
+  ins {
+    color: blue;
+    font-size: 50px;
+  }
+  #test {
+    color: blue;
+  }
+</style>
