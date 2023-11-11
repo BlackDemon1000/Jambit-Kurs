7d69ee96e4f950741e968e3d4eb3303d7933644c
BlackDemon1000 <gw1_10@gmx.de>
2023-11-11 13:05:24 +0100

    further changes

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
     let page = "default";
     let tempUsername = "default";
     let tempPassword = "default";
+    let currentUser = "Login in to see your Username";
     function saveUsername() {
         const textInput = document.getElementById('UsernameInput');
         tempUsername = UsernameInput.value;
@@ -11,21 +12,46 @@
         const textInput =document.getElementById('PasswordInput');
         tempPassword = PasswordInput.value;
     }
+    function loginButtonclickt() {
+        page = "default";
+    }
+    function registerButtonclickt() {
+        page = "register";
+    }
     /*Kommentar*/
     
 </script>
 
-<!--Kommentar-->
+<!--default header page-->
+    <button on:click={loginButtonclickt}>Login</button>
+    <button on:click={registerButtonclickt}>Register</button>
+    <button class ="changedButton">{currentUser}</button>
+
+<!--custom pages-->
 {#if page === "default"}
     <div>Username:</div>
     <input type="text" id="UsernameInput">
     <div>Password:</div>
     <input type="text" id="PasswordInput">
     <br/>
-    <button on:click={saveUsername}>Speichern</button>
+    <button on:click={saveUsername}>Login</button>
     <hr/>
     <div>Du hast {tempUsername} gespeichert</div>
     <div>Du hast {tempPassword} gespeichert</div>
+{/if}
+
+{#if page === "register"}
+    <div>Username:</div>
+    <input type="text" id="UsernameInput">
+    <div>Password:</div>
+    <input type="text" id="PasswordInput">
+    <br/>
+    <button on:click={saveUsername}>Register</button>
+    <hr/>
+{/if}
+
+{#if page === "LogedIn"}
+    <h1>Loged In succesfully with Username {currentUser}</h1>
 {/if}
 
 
@@ -37,21 +63,8 @@
 <br />
 
 <style>
-  h1 {
-    color: "red";
-  }
-
-  del {
-    background-color: green;
-  }
-
-  ins {
-    color: blue;
-    font-size: 50px;
-  }
-  /*Hier auch ein Kommentar*/
-  #test {
-    color: blue;
+  .changedButton {
+    margin-left: 300px;
   }
 </style>
 
